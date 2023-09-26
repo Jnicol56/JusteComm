cfbb1c779ce4557764698cf9287175b078d39729
Jnicol56 <133825516+Jnicol56@users.noreply.github.com>
2023-09-25 23:36:00 -0400

    9-25 updates

--- app.js
+++ app.js
@@ -8,4 +8,27 @@ $("#search-icon").click(function() {
      $(".nav").toggleClass("mobile-nav");
      $(this).toggleClass("is-active");
   });
+
   
+  $(document).ready(function() {
+   // Handle form submission
+   $("#contact-form").submit(function(e) {
+     e.preventDefault();
+     const formData = $(this).serialize();
+ 
+     // Send form data to your server
+     $.ajax({
+       type: "POST",
+       url: 'YOUR_SERVER_ENDPOINT', // Replace with your server endpoint
+       data: formData,
+       success: function(response) {
+         // Handle success, e.g., show a success message
+         alert("Message sent successfully!");
+       },
+       error: function(error) {
+         // Handle error, e.g., show an error message
+         alert("Error sending message. Please try again later.");
+       }
+     });
+   });
+ });
