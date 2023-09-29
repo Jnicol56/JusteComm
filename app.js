84d5990931571f546c23efad6276b5c33002b677
Jnicol56 <133825516+Jnicol56@users.noreply.github.com>
2023-09-28 22:20:45 -0400

    9-28
    latest

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-$("#search-icon").click(function() {
-    $(".nav").toggleClass("search");
-    $(".nav").toggleClass("no-search");
-    $(".search-input").toggleClass("search-active");
-  });
-  
-  $('.menu-toggle').click(function(){
-     $(".nav").toggleClass("mobile-nav");
-     $(this).toggleClass("is-active");
-  });
+$(document).ready(function() {
+  // Function to display a thank you message
+  function showThankYouMessage() {
+    // Hide the form
+    var form = document.getElementById('contact-form');
+    form.style.display = 'none';
 
-  
-  $(document).ready(function() {
-   // Handle form submission
-   $("#contact-form").submit(function(e) {
-     e.preventDefault();
-     const formData = $(this).serialize();
- 
-     // Send form data to your server
-     $.ajax({
-       type: "POST",
-       url: 'YOUR_SERVER_ENDPOINT', // Replace with your server endpoint
-       data: formData,
-       success: function(response) {
-         // Handle success, e.g., show a success message
-         alert("Message sent successfully!");
-       },
-       error: function(error) {
-         // Handle error, e.g., show an error message
-         alert("Error sending message. Please try again later.");
-       }
-     });
-   });
- });
+    // Display the thank you message
+    var thankYouMessage = document.getElementById('thank-you-message');
+    thankYouMessage.style.display = 'block';
+  }
+
+  // Handle form submission
+  $("#contact-form").submit(function(e) {
+    e.preventDefault();
+    const formData = $(this).serialize();
+
+    // Send form data to your server (replace 'YOUR_SERVER_ENDPOINT' with your actual server endpoint)
+    $.ajax({
+      type: "POST",
+      url: 'YOUR_SERVER_ENDPOINT',
+      data: formData,
+      success: function(response) {
+        // Handle success, e.g., show a success message
+        alert("Message sent successfully!");
+        showThankYouMessage(); // Display thank you message
+      },
+      error: function(error) {
+        // Handle error, e.g., show an error message
+        alert("Error sending message. Please try again later.");
+      }
+    });
+  });
+});
