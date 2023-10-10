fd662371b3e9a3d9e6a79d2e368012577daebb3b
Jnicol56 <133825516+Jnicol56@users.noreply.github.com>
2023-10-10 09:33:50 -0400

    10-10 updates

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
-$(document).ready(function() {
-  // Function to display a thank you message
-  function showThankYouMessage() {
-    // Hide the form
-    var form = document.getElementById('contact-form');
-    form.style.display = 'none';
+$("#search-icon").click(function() {
+  $(".nav").toggleClass("search");
+  $(".nav").toggleClass("no-search");
+  $(".search-input").toggleClass("search-active");
+});
 
-    // Display the thank you message
-    var thankYouMessage = document.getElementById('thank-you-message');
-    thankYouMessage.style.display = 'block';
-  }
-
-  // Handle form submission
-  $("#contact-form").submit(function(e) {
-    e.preventDefault();
-    const formData = $(this).serialize();
-
-    // Send form data to your server (replace 'YOUR_SERVER_ENDPOINT' with your actual server endpoint)
-    $.ajax({
-      type: "POST",
-      url: 'YOUR_SERVER_ENDPOINT',
-      data: formData,
-      success: function(response) {
-        // Handle success, e.g., show a success message
-        alert("Message sent successfully!");
-        showThankYouMessage(); // Display thank you message
-      },
-      error: function(error) {
-        // Handle error, e.g., show an error message
-        alert("Error sending message. Please try again later.");
-      }
-    });
-  });
+$('.menu-toggle').click(function(){
+   $(".nav").toggleClass("mobile-nav");
+   $(this).toggleClass("is-active");
 });
